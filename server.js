9fc64dc4c991640fb1195138739b86680171e123
Gijs <gijstenberg@gmail.com>
2025-06-03 10:34:16 +0200

    Fix code

--- server.js
+++ server.js
@@ -10,17 +10,37 @@ app.use(express.static("."));
 const io = new Server(http, { cors: { origin: "*" } });
 
 io.on("connection", (socket) => {
-    socket.on("join-room", (roomCode) => {
+    socket.on("join-room", async ({ roomCode }) => {
+        console.log(`Socket ${socket.id} joining room ${roomCode}`);
+
         for (const room of socket.rooms) {
             if (room !== socket.id) {
                 socket.leave(room);
             }
         }
+
         socket.join(roomCode);
+        socket.data.roomCode = roomCode;
+
+        socket.emit("join-success");
+
+        const sockets = await io.in(roomCode).fetchSockets();
+        console.log(`Room ${roomCode} now has ${sockets.length} clients`);
+        io.in(roomCode).emit("room-size", sockets.length);
     });
 
     socket.on("phone-button-pressed", ({ roomCode, payload }) => {
+        console.log(`Phone button pressed for room ${roomCode}`, payload);
         socket.to(roomCode).emit("trigger-pc-action", payload);
+    });
+
+    socket.on("disconnect", async () => {
+        const roomCode = socket.data.roomCode;
+        if (roomCode) {
+            const sockets = await io.in(roomCode).fetchSockets();
+            io.in(roomCode).emit("room-size", sockets.length);
+            console.log(`Socket ${socket.id} disconnected. Room ${roomCode} now has ${sockets.length} clients`);
+        }
     });
 });
 
